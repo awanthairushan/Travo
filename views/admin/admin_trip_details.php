f5bafd083dd4442a25efbf81b7dfe6d6e26a01c8
Viranga Gunarathna <virangagunarathna98@gmail.com>
2022-03-29 00:50:51 +0530

    Admin - trips more

--- views/admin/admin_trip_details.php
+++ views/admin/admin_trip_details.php
@@ -1,11 +1,11 @@
 <?php
-if (isset($_SESSION['username'])) {
-  if (mysqli_num_rows($this->isAdmin) === 1) {
+// if (isset($_SESSION['username'])) {
+//   if (mysqli_num_rows($this->isAdmin) === 1) {
 ?>
 
 <html>
     <head>
-      <style> <?php include '../../../public/css/admin/admin_trip_details.css'; ?> </style>
+      <style> <?php include APPROOT.'/public/css/admin/admin_trip_details.css'; ?> </style>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>TRIPS</title>
         <link rel="icon" href="../../../public/images/icons/favicon.ico">
@@ -19,30 +19,78 @@ if (isset($_SESSION['username'])) {
     <!--End Navigation bar-->
             <br>
             <div class="content">
-                <div class="trip">
-                    <div class="tripmenu"><a class="link" href="#container_details">TRIP</a></div>
-                    <div class="tripmenu"><a class="link" href="#container_budget">BUDGET</div>
-                    <div class="tripmenu"><a class="link" href="#container_route">ROUTE</a></div>
-                </div>
+
 <!-- ....................trip details.................... -->
+
+    <h1 class="heading-one"> TRIP DETAILS </h1>
                 <div class="container_details" id="container_details">
                     <div class="details">
                         <table class="main">
+                        <?php
+                            while ($rows = mysqli_fetch_array($this->tripDetails)){
+                                $id = $rows['trip_id'];
+                                $travelerId = $rows['traveler_id'];
+                                $start = $rows['start_date'];
+                                $end = $rows['end_date'];
+                                $count = $rows['no_of_people'];
+                                $category = $rows['category'];
+                                $mileage = $rows['mileage'];
+                                $des1 = $rows['destination_id'];
+                                $des2 = $rows['destination_id2'];
+                                $des3 = $rows['destination_id3'];
+                                $sight = $rows['sight_id'];
+                            }
+
+                            while ($rows2 = mysqli_fetch_array($this->traveler)){
+                                $name = $rows2['name'];
+                                $email = $rows2['email'];
+                                $contact1 = $rows2['contact1'];
+                                $contact2 = $rows2['contact2'];
+                            }
+
+                            while ($rows3 = mysqli_fetch_array($this->firstHotel)){
+                                $hotel1 = $rows3['name'];
+                            }
+                            while ($rows4 = mysqli_fetch_array($this->secondHotel)){
+                                $hotel2 = $rows4['name'];
+                            }
+
+                        ?>
                             <tr>
-                                <td>Date : </td>
-                                <td>2021-10-25 To 2021-10-27</td>
+                                <td>Trip ID : </td>
+                                <td><?php echo $id;?></td>
+                            </tr>
+                            <tr>
+                                <td>Traveler ID : </td>
+                                <td><?php echo $travelerId;?></td>
+                            </tr>
+                            <tr>
+                                <td>Traveler Name : </td>
+                                <td><?php echo $name;?></td>
+                            </tr>
+                            <tr>
+                                <td>Traveler Email : </td>
+                                <td><?php echo $email;?></td>
+                            </tr>
+                            <tr>
+                                <td>Traveler Contacts : </td>
+                                <td><?php echo $contact1." ".$contact2;?></td>
+                            </tr>
+                            <tr>
+                                <td>Trip Dates : </td>
+                                <td><?php echo $start;?> To <?php echo $end;?></td>
                             </tr>
                             <tr>
                                 <td>Number of Travelers : </td>
-                                <td>4</td>
+                                <td><?php echo $count;?></td>
                             </tr>
                             <tr>
                                 <td>Category : </td>
-                                <td>Cultural</td>
+                                <td><?php echo $category;?></td>
                             </tr>
                             <tr>
                                 <td>Mileage : </td>
-                                <td>358km</td>
+                                <td><?php echo $mileage;?> km</td>
                             </tr>
                         </table>
 
@@ -55,22 +103,22 @@ if (isset($_SESSION['username'])) {
                             </tr>
                             <tr>
                                 <th class="row">Destination</th>
-                                <td class="trow">Anuradhapura</td>
-                                <td class="trow">Polonnaruwa</td>
-                                <td class="trow">Kegalle</td>
+                                <td class="trow"><?php echo $des1;?></td>
+                                <td class="trow"><?php echo $des2;?></td>
+                                <td class="trow"><?php echo $des3;?></td>
                             </tr>
                             <tr>
                                 <th class="row">Hotel</th>
-                                <td class="trow">Araliya Resort</td>
-                                <td class="trow">Tamarind</td>
-                                <td class="trow">Alakamandawa</td>
+                                <td class="trow"><?php echo $hotel1;?></td>
+                                <td class="trow"><?php echo $hotel2;?></td>
+                                <td class="trow">No</td>
                             </tr>
-                            <tr>
+                            <!-- <tr>
                                 <th class="row">Sights</th>
                                 <td class="trow">Thuparamaya<br/>Isurumuniya<br/>Ruwanweliseya</td>
                                 <td class="trow">Nelum Pokuna<br/>Gal Viharaya<br/>Hatadage</td>
                                 <td class="trow">Maha Viharaya<br/>Maligawa<br/>Ashokaramaya</td>
-                            </tr>
+                            </tr> -->
                         </table>
                     </div>
                 </div>
@@ -80,36 +128,49 @@ if (isset($_SESSION['username'])) {
 <div class="container_budget" id="container_budget">
             <div class="details">
               <br>
+              
                 <table class="main">
+
+                <?php
+                    while ($rows = mysqli_fetch_array($this->budget)){
+                        $hotel1 = $rows['hotel1_accomodation'];
+                        $hotel2 = $rows['hotel2_accomodation'];
+                        $hotel3 = $rows['hotel3_accomodation'];
+                        $total = $rows['total_expenses'];
+                        $accomodations = $rows['accomodation'];
+                        $serviceCharge = $rows['service_charges'];
+                        $tickets = $rows['ticket_fees'];
+                    }
+                 ?>
                     <tr>
                         <td>Hotel 1</td>
                         <td>=</td>
-                        <td>RS 3500.00</td>
+                        <td>RS <?php echo $hotel1; ?></td>
                     </tr>
                     <tr>
                         <td>Hotel 2</td>
                         <td>=</td>
-                        <td>RS 4500.00</td>
+                        <td>RS <?php echo $hotel2; ?></td>
                     </tr>
                     <tr>
                         <td>hotel 3</td>
                         <td>=</td>
-                        <td>RS 5000.00</td>
+                        <td>RS <?php echo $hotel3; ?></td>
                     </tr>
                     <tr>
                         <td style="font-weight:bold;">Accomodations</td>
                         <td>=</td>
-                        <td>RS 13000.00</td>
+                        <td>RS <?php echo $accomodations; ?></td>
                     </tr>
                     <tr>
                         <td>Service Charges</td>
                         <td>=</td>
-                        <td>RS 1000.00</td>
+                        <td>RS <?php echo $serviceCharge; ?></td>
                     </tr>
                     <tr>
                         <td>Ticket fees</td>
                         <td>=</td>
-                        <td>(Rs 500.00)</td>
+                        <td>(Rs <?php echo $tickets; ?>)</td>
                     </tr>
                     <tr>
                         <th class="row" style="font-weight:bold;">Total Budget</th>
@@ -119,15 +180,10 @@ if (isset($_SESSION['username'])) {
                 </table>
             </div>
         </div>
+      
 <!-- .............................end of trip budget.......................... -->
 
-<!-- .............................trip route.......................... -->
-<div class="container_route" id="container_route">
-            <div class="details">
-              <br>
-                <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7936.595061707961!2d80.5334359!3d5.953681200000001!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2slk!4v1629276519410!5m2!1sen!2slk" width="1000" height="360" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-            </div>
-        </div>
+
 
 <!-- .............................end of trip route.......................... -->
             </div>
@@ -136,12 +192,12 @@ if (isset($_SESSION['username'])) {
     </body>
 </html>
 <?php
-  } else {
-    echo '<script type="text/javascript">javascript:history.go(-1)</script>';
-    exit();
-  }
-} else {
-  header("location: http://localhost/TRAVO");
-  exit();
-}
+//   } else {
+//     echo '<script type="text/javascript">javascript:history.go(-1)</script>';
+//     exit();
+//   }
+// } else {
+//   header("location: http://localhost/TRAVO");
+//   exit();
+// }
 ?>
